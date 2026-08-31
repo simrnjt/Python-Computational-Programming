eaa66be8189597cd63fc9b7d2d3dc85a53a5b491
SimranjeetSingh <simranjeet777000@gmail.com>
2026-08-31 21:31:24 +0530

    added more code in code1.py

--- _1_basics & data types/code1.py
+++ _1_basics & data types/code1.py
@@ -5,8 +5,37 @@
 # 4. booleans
 # 5. strings
 
-
+#integer data type
 print(5)
 print(1024)
-
 print(type(45))
+print('----------------------------')
+print('\n')
+
+#float data type
+print(3.5)
+print(type(1.3))
+print('----------------------------')
+print('\n')
+
+#complex numbers data type
+print(2+7j)
+print(type(7+5j))
+print('----------------------------')
+print('\n')
+
+#boolean data type i.e. True or False boolean value
+print(True)
+print(False)
+print(type(True))
+print(type(False))
+print('----------------------------')
+print('\n')
+
+#string data type. string can be denoted by single or double quotes
+print('a')
+print('7')
+print(type('76'))
+print('Orange')
+print("Japan")
+print(type('Tokyo'))
