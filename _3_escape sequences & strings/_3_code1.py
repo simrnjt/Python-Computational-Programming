483ad1c55239d3981b9a9f410611910818c9eddf
simrnjt <simranjeet555singh@gmail.com>
2026-08-31 23:56:24 +0530

    made a new folder _3_escape sequences & strings and within this folder made a new code file _3_code1.py

---
+++ _3_escape sequences & strings/_3_code1.py
@@ -0,0 +1,34 @@
+#single quotes and double quotes strings
+print('hello china')
+print("hello china")
+print("hello 'japan'")
+print('hello "singapore"')
+print('\n')
+
+# \' escape sequences is used in the case of single quotes
+# \'' escape sequences is used in the case of double quotes
+print('hi, I\'m Rohan')
+print("hi, I\'m Alice")
+print('yellow \'car\'')
+print("Hi, \"Switzerland\"")
+print('\n')
+
+#escape sequences for a new line('\n')
+#this creates a new line between the two words Milkeyway and Galaxy
+print('Milkeyway\nGalaxy')
+print('tokyo \nuniversity')
+print('osaka \n university')
+print('okinawa\n institute')
+print('\n')
+
+#tab escape sequence(\t)
+print('name:\tSamarth')
+print('course:\toperating system')
+print('age:\t19')
+print('\n')
+
+#backspace escape sequence (\b)
+# The cursor moves back over the 'o', and 'o' is overwritten by 'W'
+print("Hello\bWorld")
+# Output in most terminals: HellWorld
+print('\n')
