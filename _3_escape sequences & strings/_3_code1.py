170e42570c2e054bd7bbf2e2437dd3a00a0f8ff9
simrnjt <simranjeet555singh@gmail.com>
2026-09-01 10:09:07 +0530

    made a new code file _3_code2.py to play more with escape sequences and strings

--- _3_escape sequences & strings/_3_code1.py
+++ _3_escape sequences & strings/_3_code1.py
@@ -3,6 +3,7 @@ print('hello china')
 print("hello china")
 print("hello 'japan'")
 print('hello "singapore"')
+print("--------------------------------------------------------")
 print('\n')
 
 # \' escape sequences is used in the case of single quotes
@@ -11,6 +12,7 @@ print('hi, I\'m Rohan')
 print("hi, I\'m Alice")
 print('yellow \'car\'')
 print("Hi, \"Switzerland\"")
+print("----------------------------------------------------------")
 print('\n')
 
 #escape sequences for a new line('\n')
@@ -19,16 +21,31 @@ print('Milkeyway\nGalaxy')
 print('tokyo \nuniversity')
 print('osaka \n university')
 print('okinawa\n institute')
+print("-----------------------------------------------------------")
 print('\n')
 
 #tab escape sequence(\t)
 print('name:\tSamarth')
 print('course:\toperating system')
 print('age:\t19')
+print("----------------------------------------------------------")
 print('\n')
 
 #backspace escape sequence (\b)
 # The cursor moves back over the 'o', and 'o' is overwritten by 'W'
 print("Hello\bWorld")
 # Output in most terminals: HellWorld
+print("------------------------------------------------------------")
 print('\n')
+
+#double backslash escape sequences
+#to print a single backslash, use two backslashes
+print('\\')
+#to print two backslashes, use four backslashes
+print('\\\\')
+print('\\\\\\')
+#using raw string i.e. r, to treat the backslashes not as a backslash but only as a string
+print(r'\\')
+print(r"\\")
+#here, we are combining the raw strig with the normal backslash printing
+print(r"\\" + "\\")
---
+++ _3_escape sequences & strings/_3_code2.py
@@ -0,0 +1,10 @@
+#treating escape sequences as a normal text
+#i.e.print the new line escape sequence (\n) as a normal text only
+print('King-1 \\n King-2')
+#now, print the backslash and the second text part on a new line
+print('yellow1 \\\n yellow2')
+print('------------------------------------------------')
+print('\n')
+
+#usage of new line(\n) escape sequences to print a full sentence
+print("Hi, My name is 'Roy'\nI'm 19 years old\nI'm 'Chinese'")
