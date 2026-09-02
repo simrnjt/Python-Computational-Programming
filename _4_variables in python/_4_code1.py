7606f96f88b9afcadf10ef5580c5708505d0f78f
simrnjt <simranjeet555singh@gmail.com>
2026-09-02 19:19:02 +0530

    made a new folder for variables topic

---
+++ _4_variables in python/_4_code1.py
@@ -0,0 +1,23 @@
+#variables a and b are storing integer type of values
+a = 5
+b = 7
+print(a * b)
+print('---------------------------------------------------------')
+print('\n')
+
+
+#changing the value of variable a to a float value and b as an integer value
+a = 1.2
+b = 3
+print(a + b)
+print('---------------------------------------------------------')
+print('\n')
+
+
+#changing the value to complex values
+a = 3.54
+b = 2 + 7j
+print(a + b)
+print('---------------------------------------------------------')
+print('\n')
+
