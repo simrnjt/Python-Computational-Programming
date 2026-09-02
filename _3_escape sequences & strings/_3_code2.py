052fcbede69fa7d471c8a9628186503c53e71d6f
simrnjt <simranjeet555singh@gmail.com>
2026-09-02 19:18:30 +0530

    added few code into code file

--- _3_escape sequences & strings/_3_code2.py
+++ _3_escape sequences & strings/_3_code2.py
@@ -8,8 +8,50 @@ print('yellow1 \\\n yellow2')
 print('------------------------------------------------')
 print('\n')
 
+
+#print single apostrophe symbol i.e. single quote
+#here, we have first print the single quote using single quotes at the end points.
+print('\'')
+#here, we have first print the single quote using double quotes at the end points.
+print("\'")
+print('------------------------------------------------')
+print('\n')
+
+
+#print double apostrophe symbol i.e. double quotes
+#here, we have first print the double quote using single quotes at the end points.
+print('\"')
+#here, we have first print the double quote using double quotes at the end points.
+print("\"")
+print('------------------------------------------------')
+print('\n')
+
+
+#print single quote and double quote together
+print('\" \'')
+print("\" \'")
+print('------------------------------------------------')
+print('\n')
+
+
+#print backslash with double apostrophe & backslash with single apostrohpe
+print('\\\" \\\'')
+print('------------------------------------------------')
+print('\n')
+
+
+#print backslash with double apostrophe & backslash with single apostrohpe using Raw Strings Concept
+print(r'\" \'')
+print('------------------------------------------------')
+print('\n')
+
+
 #usage of new line(\n) escape sequences to print a full sentence
 print("Hi, My name is 'Roy'\nI'm 19 years old\nI'm 'Chinese'")
+print('------------------------------------------------')
+print('\n')
 
+
+#just print these sentences
 print("hi, i'm from USA\n")
 print("hello, i'm from japan\n")
