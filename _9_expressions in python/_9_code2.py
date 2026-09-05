cc53dccc55c791d61e80d1ed5a0bf9773ee8aeb9
simrnjt <simranjeet555singh@gmail.com>
2026-09-05 12:39:12 +0530

    associativity and precendence in python

---
+++ _9_expressions in python/_9_code2.py
@@ -0,0 +1,10 @@
+#Associativity and Precedence in expression
+print(3%4)
+expression1 = 2 + 3 % 4 * 2
+print(expression1)
+print('---------------------------------------------')
+print('\n')
+
+
+print(2 * 4 / 5)
+print(1 % 2 ** 3 * 2 + 1)
