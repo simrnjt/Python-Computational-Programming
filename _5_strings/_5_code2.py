625276016b8bfe9c7e645925d4ad0e24ffc5853c
simrnjt <simranjeet555singh@gmail.com>
2026-09-05 09:15:42 +0530

    adding few more code snippets of strings

--- _5_strings/_5_code2.py
+++ _5_strings/_5_code2.py
@@ -13,3 +13,18 @@ age = 10
 weight = 50
 print(f'yesterday, I saw a one boy with age {age} and weight {weight}.')
 print(f'yesterday, I saw a one boy with age {age-3} and weight {weight+20}.')
+print('---------------------------------------------------')
+print('\n')
+
+
+#string concatenation
+s1 = '5'
+s2 = '4'
+print(s1+s2)
+print('---------------------------------------------------')
+print('\n')
+
+
+#string duplication
+s3 = '7'
+print(s3*5)
